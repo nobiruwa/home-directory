fcd20a7d8ec63e11414af8d30dcf462c108c6ca5
ein <ein@hpnull2>
2017-11-25 09:45:42 +0900

    add stack-install

--- create-gitignore.sh
+++ create-gitignore.sh
@@ -7,12 +7,12 @@ find . -maxdepth 1 -name ".*" -type f | sed -e 's|^\.\/||' | sort | sed -e 's|^|
 echo >> .gitignore
 find . -maxdepth 1 -type f | sed -e 's|^\.\/||' | egrep "^[^.]" | sort | sed -e 's|^|!|' >> .gitignore
 
-for subdir in `ls -d .[^.]*/ | grep -v \.git`; do
+for subdir in `ls -d .[^.]*/ | grep -v \.git | sort`; do
     echo >> .gitignore
-    find $subdir | sed -e 's|^.\/||' | sed -e 's|^|!|' >> .gitignore
+    find $subdir | sed -e 's|^.\/||' | sed -e 's|^|!|' | sort >> .gitignore
 done
 
-for subdir in `ls -d [^.]*/`; do
+for subdir in `ls -d [^.]*/ | sort`; do
     echo >> .gitignore
-    find $subdir | grep -v \.git | sed -e 's|^.\/||' | sed -e 's|^|!|' >> .gitignore
+    find $subdir | grep -v \.git | sed -e 's|^.\/||' | sed -e 's|^|!|' | sort >> .gitignore
 done
