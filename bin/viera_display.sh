7fc5ff284cf24f33a388ac24f9652ed8893b3d83
ein <ein@hpnull2>
2020-02-23 19:10:55 +0900

    viera_display.sh can specify monitor position with making 1st argument 'right'.

--- bin/viera_display.sh
+++ bin/viera_display.sh
@@ -1,4 +1,11 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+if [ "$1"="right" ]; then
+    hdmi_option=( --right-of )
+else
+    hdmi_option=( --left-of )
+fi
+
 
 xrandr --output LVDS-1 --mode 1366x768
-xrandr --output HDMI-1 --mode 1280x720 --left-of LVDS-1
+xrandr --output HDMI-1 --mode 1280x720 "${hdmi_option[@]}" LVDS-1
