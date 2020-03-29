cd6eb38dd275163ee84722a1c4af7c5338ef4bbf
ein <ein@hpnull.localdomain>
2020-03-29 18:38:57 +0900

    add build-ccls.sh

---
+++ bin/build-ccls.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+CCLS_DIR=$HOME/repo/ccls.git
+CCLS_URL=https://github.com/MaskRay/ccls
+
+function clone {
+    git clone --depth=1 --recursive "${CCLS_URL}" "${CCLS_DIR}"
+}
+
+if [ x"rebuild" = x"$1" ]; then
+    if [ -d "$CCLS_DIR"]; then
+        mv "$CCLS_DIR" "/tmp/${CCLS_DIR}.old"
+    fi
+    clone
+fi
+
+if [ ! -d "${CCLS_DIR}" ]; then
+    clone
+fi
+
+cd "${CCLS_DIR}" && cmake -H. -Brelease -DCMAKE_BUILD_TYPE=Release && cmake --build release
