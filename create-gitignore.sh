4d5146d56525f98fac9d2e06117224055b072a4e
ein <ein@thinknull>
2022-03-16 16:11:49 +0900

    update

--- create-gitignore.sh
+++ create-gitignore.sh
@@ -1,18 +1,51 @@
-mv .gitignore /tmp/gitignore
-echo '*' > .gitignore
+#!/usr/bin/env bash
 
-echo >> .gitignore
-find . -maxdepth 1 -name ".*" -type f | sed -e 's|^\.\/||' | sort | sed -e 's|^|!|' >> .gitignore
+function shallow-search() {
+    search_result=`find "${CURRENT_SEARCH_DIR}" -mindepth 1 -maxdepth 1 | grep -v ".git$" | sort`
+}
 
-echo >> .gitignore
-find . -maxdepth 1 -type f | sed -e 's|^\.\/||' | egrep "^[^.]" | sort | sed -e 's|^|!|' >> .gitignore
+function deep-search() {
+    search_result=`find "${CURRENT_SEARCH_DIR}" -mindepth 1 | grep -v ".git$" | sort`
+}
 
-for subdir in `ls -d .[^.]*/ | grep -v \.git | sort`; do
-    echo >> .gitignore
-    find $subdir | sed -e 's|^.\/||' | sed -e 's|^|!|' | sort >> .gitignore
-done
+SEARCH_DIR=${1:-.}
 
-for subdir in `ls -d [^.]*/ | sort`; do
-    echo >> .gitignore
-    find $subdir | grep -v \.git | sed -e 's|^.\/||' | sed -e 's|^|!|' | sort >> .gitignore
-done
+if [ "${SEARCH_DIR}" = "." ]; then
+    echo '*'
+    prev=asterisk
+
+    CURRENT_SEARCH_DIR="${SEARCH_DIR}"
+    shallow-search
+    top_children=$search_result
+
+    for child in $top_children; do
+
+        if [ -L "$child" -o -f "$child" ]; then
+            if [ "$prev" != "file" ]; then
+                echo ''
+            fi
+
+            echo "$child" | sed -e 's|^\./|!|'
+
+            prev=file
+            continue
+        else
+            echo ''
+            echo "$child" | sed -e 's|^\./|!|'
+
+            CURRENT_SEARCH_DIR="$child"
+            deep-search
+            echo "$search_result" | sed -e 's|^\./|!|'
+
+            prev=directory
+        fi
+    done
+else
+    echo "${SEARCH_DIR}" | sed -e 's|^|!|' | sed -e 's|\/$||'
+
+    CURRENT_SEARCH_DIR="${SEARCH_DIR}"
+    deep-search
+    echo "$search_result" | sed -e 's|^|!|'
+fi
+
+
