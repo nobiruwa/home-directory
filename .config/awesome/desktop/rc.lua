58029097f507cf1a53996528c49f70b30bfc196b
ein <ein@thinknull2>
2025-11-16 20:15:56 +0900

    updated.

--- .config/awesome/desktop/rc.lua
+++ .config/awesome/desktop/rc.lua
@@ -563,6 +563,10 @@ awful.rules.rules = {
     -- Set Firefox to always map on the tag named "2" on screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { screen = 1, tag = "2" } },
+
+    -- Set Firefox non-maximized and non-floating on startup
+    { rule = { class = "firefox" },
+      properties = { maximized = false, floating = false } },
 }
 -- }}}
 
